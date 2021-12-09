2cf88c9e0fe30fa3ca6f66f74b12a51cbf1b5c45
Josh <joshuasitumorang0203@gmail.com>
2021-12-09 09:28:59 +0700

    Change Log : liquid animation

--- lib/feature/flutter_chart/flutter_chart_screen.dart
+++ lib/feature/flutter_chart/flutter_chart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_research/styles/fontTheme.dart';
 
 class FlutterChartScreen extends StatefulWidget {
   @override
@@ -8,8 +9,58 @@ class FlutterChartScreen extends StatefulWidget {
 class _FlutterChartScreenState extends State<FlutterChartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: new BorderRadius.only(
+                  bottomLeft: const Radius.circular(40.0),
+                  bottomRight: const Radius.circular(40.0),
+                ),
+                color: Color(0xFFFFCF86),
+              ),
+              height: MediaQuery.of(context).size.height * 0.36,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.asset(
+                          'assets/chartVector.png',
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                      Positioned(
+                          top: 35,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            child: Text(
+                              'FLUTTER CHART',
+                              style: titleHeaderWhiteLSpacing,
+                              textAlign: TextAlign.center,
+                            ),
+                          )),
+                    ],
+                  ),
+                  Container(
+                      width: 208, child: Image.asset('assets/chartImage.png'))
+                ],
+              ),
+            ),
+            Container(
+              child: Expanded(
+                child: ListView(
+                  children: [],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/styles/fontTheme.dart
+++ lib/styles/fontTheme.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextStyle titleHeader =
     GoogleFonts.openSans(fontWeight: FontWeight.bold, fontSize: 26);
 
+TextStyle titleHeaderWhite = GoogleFonts.openSans(
+    fontWeight: FontWeight.bold, fontSize: 26, color: Colors.white);
+
+TextStyle titleHeaderWhiteLSpacing = GoogleFonts.openSans(
+    fontWeight: FontWeight.bold,
+    fontSize: 32,
+    color: Colors.white,
+    letterSpacing: 2.0);
+
 TextStyle titleSubHeader =
     GoogleFonts.openSans(fontWeight: FontWeight.w300, fontSize: 20);
+
+TextStyle titleSubHeaderWhite = GoogleFonts.openSans(
+    fontWeight: FontWeight.w300, fontSize: 20, color: Colors.white);
 
 TextStyle titleCardWhite = GoogleFonts.openSans(
     fontWeight: FontWeight.bold,
@@ -15,3 +28,21 @@ TextStyle titleCardWhite = GoogleFonts.openSans(
 
 TextStyle titleCardBlack = GoogleFonts.openSans(
     fontWeight: FontWeight.bold, fontSize: 26, color: Color(0xFF3F414E));
+
+TextStyle sans16WhiteBold = GoogleFonts.openSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Colors.white,
+    letterSpacing: 1.5);
+
+TextStyle sans16White = GoogleFonts.openSans(
+    fontWeight: FontWeight.w300,
+    fontSize: 16,
+    color: Colors.white,
+    letterSpacing: 1.0);
+
+TextStyle sans16Black = GoogleFonts.openSans(
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+    color: Colors.black,
+    letterSpacing: 1.0);
